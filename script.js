ca8b317bce6a36d9150a3a7746a0cdfcdaea4569
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-09-30 16:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,28 +21,50 @@ prev.addEventListener('click',()=>{
 	}
 	update();
 });
-function update(){
-	circles.forEach((circle,idx)=>{
-		if(idx<currentActive){
-			circle.classList.add('active')
-		}
-		else{
-			circle.classList.remove('active')
-		}
-	})
-	const actives=document.querySelectorAll('.active');
-	progress.style.width=((actives.length-1)/(circle.length-1))*100+'%';
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
 
-	if(currentActive===1){
-		prev.disabled=true;
-	}
-	else{
-		prev.disabled=false;
-	}
-	if(currentActive===circles.length){
-		next.disabled=true;
-	}
-	else{
-		next.disabled=false;
-	}
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else {
+        prev.disabled = false;
+    }
+    if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        next.disabled = false;
+    }
 }
+// function update(){
+// 	circles.forEach((circle,idx)=>{
+// 		if(idx<currentActive){
+// 			circle.classList.add('active')
+// 		}
+// 		else{
+// 			circle.classList.remove('active')
+// 		}
+// 	})
+// 	const actives=document.querySelectorAll('.active');
+// 	progress.style.width=((actives.length-1)/(circle.length-1))*100+'%';
+
+// 	if(currentActive===1){
+// 		prev.disabled=true;
+// 	}
+// 	else{
+// 		prev.disabled=false;
+// 	}
+// 	if(currentActive===circles.length){
+// 		next.disabled=true;
+// 	}
+// 	else{
+// 		next.disabled=false;
+// 	}
+// }
